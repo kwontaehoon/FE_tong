e3a4a803247f44a9208a8f2e3a424458961b1a17
nayoung33 <ilnayungli@gmail.com>
2024-01-31 22:32:20 +0900

    feat: 로그인 추가 및 마이페이지 작업중

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { useBearStore } from '../../store/test'
-import { useTestQuery } from '../../hooks/queries/Test';
+
 
 const index = () => {
 
-    const bears = useBearStore((state) => state.bears);
-
-    const { data: test } = useTestQuery();
+    
 
   return (
     <>
-        <div>{bears}</div>
-        <div>{test}</div>
         <div>main</div>
     </>
   )
--- src/component/signup/index.jsx
+++ src/component/signup/index.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
-import { Component } from './styles'
-import { useNavigate } from 'react-router-dom'
+import { 
+    Component,
+    Header,
+    Logo,
+    Center,
+    Social,
+    Id,
+    Pwd,
+    Login,
+    ID_Mini,
+    Pwd_Mini,
+    Signup_Mini,
+    Minibox,
+    Social_Mini 
+    
+} from './styles'
 
 const index = () => {
 
-    const navigate = useNavigate();
 
     return (
         <Component>
-            <div>여기는 로그인페이지입니다.</div>
-            <button onClick={() => navigate("/login")}>로그인으로 이동</button>
+           <Header><img src="/svg/Vector.svg"/></Header>
+
+           <Logo>LOGO</Logo>
+
+           <Center>
+                <Id placeholder='아이디'></Id>
+                <Pwd placeholder='비밀번호'></Pwd>
+                <Login>로그인</Login>
+
+            < Minibox>
+                <ID_Mini>아이디 찾기</ID_Mini><img src="/svg/Line 2.svg"/>
+                <Pwd_Mini>비밀번호 찾기</Pwd_Mini><img src="/svg/Line 2.svg"/>
+                <Signup_Mini>회원가입</Signup_Mini>
+                </Minibox> 
+        
+           </Center>
+
+           <Social>
+            < Social_Mini><img src="/svg/Ellipse 19.svg"/></Social_Mini>
+            < Social_Mini><img src="/svg/Ellipse 19.svg"/></Social_Mini>
+           </Social>
+
         </Component>
     )
 }
