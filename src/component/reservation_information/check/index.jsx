de95e27d60b4529ae841e528abc69b4efec54b5d
nayoung33 <ilnayungli@gmail.com>
2024-02-13 16:39:42 +0900

    feat:예약상세

--- src/component/reservation_information/check/index.jsx
+++ src/component/reservation_information/check/index.jsx
@@ -3,7 +3,13 @@ import {Container,
 Reservation_Box,
 Reservation,
 Text,
-Check_Box
+Check_Box,
+Check,
+Check_inout ,
+Check_in,
+Days_Time,
+Days
+
 } from './styles'
 
 const index = () => {
@@ -16,7 +22,26 @@ const index = () => {
         <Text>
           Lorem ipsum dolor
         </Text>
-        <Check_Box></Check_Box>
+        <Check_Box>
+          <Check>
+           <Check_inout>
+            <Check_in>체크인</Check_in>
+            <Days_Time>
+              <Days>01.31목</Days>
+              <img src="svg/Line_5.svg"></img>
+              <Days>13:00</Days>
+            </Days_Time>
+           </Check_inout>
+           <Check_inout>
+            <Check_in>체크아웃</Check_in>
+            <Days_Time>
+              <Days>02.01목</Days>
+              <img src="svg/Line_5.svg"></img>
+              <Days>12:00</Days>
+            </Days_Time>
+           </Check_inout>
+            </Check> 
+        </Check_Box>
       </Reservation_Box>
     </Container>
   )
--- src/component/reservation_information/check/styles.js
+++ src/component/reservation_information/check/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
    
 `
 export const Reservation_Box = styled.div`
-    height: 210px;
+    height: 200px;
     border: 1px solid;
     margin-top: 20px;
 `
@@ -20,7 +20,44 @@ export const Text = styled.div`
     font-size: 13px;
 `
 export const Check_Box = styled.div`
-    height: 120px;
-    border: 1px solid;
     margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+   
+`
+export const Check = styled.div`
+    width: 290px;
+    height: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+    margin-left: 12px;
+    background-color: #F3F3F3;
+`
+export const Check_inout = styled.div`
+    width: 110px;
+    height: 60px;
+    margin-left: 6px;
+` 
+export const Check_in = styled.div`
+    text-align: center;
+    font-size: 13px;
+    color: #CECECE;
+`
+export const Days_Time = styled.div`
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 8px;
+    margin-top: 10px;
+`
+export const Days = styled.div`
+    font-size: 13px;
+    margin: 0px 10px 0px 10px;
+    white-space: nowrap;
+    
+
 `
