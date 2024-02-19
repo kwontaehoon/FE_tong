25bf38a36c26b42a064b19df5f2a3365552aa4fd
nayoung33 <ilnayungli@gmail.com>
2024-02-19 17:26:46 +0900

    feat: 예약시간선택 추가

---
+++ src/component/reservation_options/heather/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+        padding: 20px;
+`
+export const Heather = styled.div`
+    height: 48px;
+    border: 1px solid;
+    display: flex;
+    align-items: center;
+`
+export const Option = styled.div`
+    font-size: 20px;
+    font-weight: 700;
+    font-family: "Noto Sans KR";
+    flex: 1;
+    text-align: center;
+`
