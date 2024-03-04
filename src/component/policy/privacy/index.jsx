d2ce1d8bc11f961afddfa3273f6b0dcd4a9b316d
kimjungae <joni81@naver.com>
2024-03-04 15:32:50 +0900

    term

--- src/component/policy/privacy/index.jsx
+++ src/component/policy/privacy/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../../../function/header'
 
 const index = () => {
   return (
-    <div>개인정보처리방침</div>
+    <div>
+      <Header Arrow title={"개인정보처리방침"} padding/>
+
+      
+      개인정보처리방침
+      </div>
   )
 }
 
---
+++ src/component/policy/term/styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+padding:  0 0 29px;
+`
+export const Top = styled.div`
+background:#fff;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 45px;
+border-bottom: 1px solid #E9EBED;
+`
+export const Img = styled.img`
+width: 80px;
+`
+
+export const MallTerm = styled.div`
+margin:14px 7px 0; 
+padding:14px; 
+color:#777; 
+font-size:11px; 
+line-height:1.5; 
+border:1px solid #dddddd; 
+background:#fff; 
+text-align: justify;
+`
+export const Quick = styled.div`
+position: fixed;
+    bottom: 20px;
+    right: 5%;
+    z-index: 100;
+    margin: 0 0 0 -36px;
+    border: 1px solid #d6d6d7;
+    border-top: 0;
+    border-radius: 999px;
+    opacity: 0.8;
+    background: #616161 ;
+    background-size: 12px 12px;
+    padding: 2px 12px;
+    color: #fff;
+    font-size: 10px;
+    
+`
+export const BtnArea = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 12px;
+    height: 30px;
+`
+export const Button = styled.button`
+    color: #222;
+    background-color: #F7F8FE;
+    padding: 4px 16px;
+    border:1px solid #dddddd; 
+    /* border-radius: 12px; */
+    color:#777;
+    font-size: 14px;
+    line-height: 30px;
+`
